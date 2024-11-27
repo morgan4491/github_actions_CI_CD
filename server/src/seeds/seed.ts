c731db85cd6f9ecea9523353d04dc8407aaad4d4
morgan4491 <m.morgan4491@gmail.com>
2024-11-27 09:35:59 -0500

    fixed seed.ts assert error during render deploy again

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,11 +1,8 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
-import fs from 'fs';
-import path from 'path';
 
-const pythonQuestionsPath = path.resolve(__dirname, './pythonQuestions.json');
-const pythonQuestions = JSON.parse(fs.readFileSync(pythonQuestionsPath, 'utf-8'));
+import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
 
 db.once('open', async () => {
   await cleanDB();
